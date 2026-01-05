bb7cd37b201ec09a2b932cf5d500be15841abd94
neha <devineha590@gmail.com>
2026-01-05 23:43:18 +0530

    array

---
+++ array/src/com/array/Demo1.java
@@ -0,0 +1,34 @@
+package com.array;
+import java.util.Scanner;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size of element");
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		System.out.println("Enter the element in a array");
+		for(int i=0;i<=n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		System.out.println("Enter the target element");
+		int x=sc.nextInt();
+		boolean flag=false;
+		for(int i=0;i<n;i++) {
+			if(arr[i]==x) {
+				flag=true;
+				break;
+			}
+		}
+		if(flag==true) {
+			System.out.println("Element Found.......");
+		}
+		else {
+			System.out.println("Element not Found.....");
+		}
+		
+	}
+
+}
+
+
