508af7cc2b7cbf592168b7fc5e9a7706470a4212
neha <devineha590@gmail.com>
2026-04-10 21:38:23 +0530

    StringLength

---
+++ array/src/com/string/String1.java
@@ -0,0 +1,23 @@
+
+//String Length
+package com.string;
+
+import java.util.Scanner;
+
+public class String1 {
+	public static void main(String[] args) {
+		/*char arr[]= {'a','b','c','d'};
+		String str="abcd";
+		String str2=new String("xyz@#1234");
+		
+		//String are immutable
+		
+		Scanner sc=new Scanner(System.in);
+		String name;
+		name=sc.nextLine();
+		System.out.println(name);*/
+		String fullname="TonyStark";
+		System.out.println(fullname.length());
+	}
+
+}
